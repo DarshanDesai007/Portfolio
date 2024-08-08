fc94909c3195c6dca38d13624bc8d21487fd66e0
DarshanDesai007 <91963003+DarshanDesai007@users.noreply.github.com>
2024-08-08 16:22:36 +0530

    Update personal-data.js
    Update

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -2,7 +2,7 @@ export const personalData = {
   name: "Darshan Desai",
   profile: '/profile.png',
   designation: "Software Developer",
-  description: "I'm Darshan Desai, a software developer who loves working with ASP.NET and JavaScript. I'm passionate about creating new things on the web and making them better. People know me for being quick to learn and good at solving problems. I enjoy working in challenging environments that look forward to what's next. When I develop, I pay close attention to detail and use my creativity to make each project stand out. I'm excited to work with a team that shares my enthusiasm for improving user experiences online and shaping the future of web applications.",
+  description: "I'm Darshan Desai, a professional software developer who loves working with ASP.NET and JavaScript. I'm passionate about creating new things on the web and making them better. People know me for being quick to learn and good at solving problems. I enjoy working in challenging environments that look forward to what's next. When I develop, I pay close attention to detail and use my creativity to make each project stand out. I'm excited to work with a team that shares my enthusiasm for improving user experiences online and shaping the future of web applications.",
   email: 'mmmdarshan98@gmail.com',
   phone: '+91 8080205724',
   address: 'Mumbai, Maharashtra, India ',
