3fe1699c8601c9156679b779228ac7ec511de06d
DarshanDesai007 <91963003+DarshanDesai007@users.noreply.github.com>
2024-08-28 11:46:10 +0530

    Update contactsData.js
    vasai  to mumbai

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,7 +1,7 @@
 export const contactsData = {
     email: 'mmmdarshan98@gmail.com',
     phone: '+918080205724',
-    address: 'Vasai, Maharashtra, India - 401208 ',
+    address: 'Mumbai, Maharashtra, India - 401208 ',
     github: 'https://github.com/DarshanDesai007',  
     linkedIn: 'https://www.linkedin.com/in/darshandesai19/'
 }
