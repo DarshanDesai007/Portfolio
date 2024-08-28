b7b623e468b3b16d839ca034388d4d6a348a66db
DarshanDesai007 <91963003+DarshanDesai007@users.noreply.github.com>
2024-08-28 11:46:27 +0530

    Update contactsData.js

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,7 +1,7 @@
 export const contactsData = {
     email: 'mmmdarshan98@gmail.com',
     phone: '+918080205724',
-    address: 'Mumbai, Maharashtra, India - 401208 ',
+    address: 'Mumbai, Maharashtra, India',
     github: 'https://github.com/DarshanDesai007',  
     linkedIn: 'https://www.linkedin.com/in/darshandesai19/'
 }
