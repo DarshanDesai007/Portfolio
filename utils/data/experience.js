e8cb40e1253c338a410b197f5df2f2f7fb299d45
DarshanDesai007 <91963003+DarshanDesai007@users.noreply.github.com>
2025-01-21 13:04:29 +0530

    Update experience.js
    resume update

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -3,12 +3,6 @@ export const experiences = [
     id: 1,
     title: 'Software Developer',
     company: "Saksham Systems And Technologies Pvt. Ltd.",
-    duration: "(Nov 2023 - Present)"
-  },
-  {
-    id: 2,
-    title: "FullStack Developer",
-    company: "Flynetics Systems Pvt. Ltd.",
-    duration: "(Jun 2023 - Oct 2023)"
+    duration: "(Nov 2022 - Present)"
   }
 ]
